36de5134e32a43184de66d3ec936cb2b1cbac037
reed0023 <stephen.reed@showclix.com>
2021-09-03 10:39:42 -0400

    Update and rename mapanimationSolution.js to mapanimation.js

--- mapanimation.js
+++ mapanimation.js
@@ -16,7 +16,7 @@ const busStops = [
 
 // TODO: add your own access token
 mapboxgl.accessToken =
-  'pk.eyJ1IjoidGVzdHVzZXIxMDAwIiwiYSI6ImNraDkzZ2pkMzAzMHoycnBmMXpvZ3UwZnMifQ.jAE4YsPeAJv50VK92NSpOQ';
+  'pk.eyJ1IjoicmVlZDAwMjMiLCJhIjoiY2tzOTk0cG05MHhiYTJ6cm8zYXdtYjQ0MyJ9.PfEForCWcojOek2NBV1Sgg';
 
 // TODO: create the map object using mapboxgl.map() function
 let map = new mapboxgl.Map({
